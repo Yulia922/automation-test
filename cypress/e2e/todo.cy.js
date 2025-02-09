d18b492a16440b151144a9820f1c548f9bafbeb9
Julia Agapitova <julleag6436@gmail.com>
2025-02-09 14:44:30 +0400

    Update e2e test

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,5 +1,12 @@
 describe('visit todo', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io/todo')
+    const input = cy.get('[data-test="new-todo"]')
+    input.type('Buy milk')
+    input.type('{enter}')
+    const list = cy.get('.todo-list')
+    const todoitem = list.contains('Buy milk')
+    const checkbox = todoitem.prev()
+    checkbox.click()
   })
 })
